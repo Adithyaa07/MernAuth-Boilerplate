e52be2e82d6ea337e5e76987d443f9892fa1ddc0
AdithyaRaj <113771824+Adithyaa07@users.noreply.github.com>
2024-05-10 19:43:27 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Profile from "./pages/Profile.jsx";
+import Header from "./components/Header.jsx";
+
 function App() {
-  return <h1 className="text-red-700">Hello Main</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
