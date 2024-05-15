935e3c06020ddaa5640031e99f08b71ca6d07702
AdithyaRaj <113771824+Adithyaa07@users.noreply.github.com>
2024-05-15 13:58:43 +0530

    privateRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Profile from "./pages/Profile.jsx";
 import Header from "./components/Header.jsx";
-
+import PrivateRoute from "./components/PrivateRoute.jsx";
 function App() {
   return (
     <BrowserRouter>
@@ -16,7 +16,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
